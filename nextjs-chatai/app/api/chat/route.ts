f984c031c0c35b9253875f9e4ccd15b2917c479b
jorgerr9011 <jorge.rodriguez.rivera@udc.es>
2024-04-30 20:07:42 +0200

    Error en subida de archivo

--- nextjs-chatai/app/api/chat/route.ts
+++ nextjs-chatai/app/api/chat/route.ts
@@ -87,33 +87,3 @@ export async function POST(req: Request) {
     })
   }
 }
-
-// Función para borrar historial de mensajes del chat 
-export async function PUT(req: Request, { params }: any) {
-
-  try {
-
-    await connectDB()
-    const { email } = params
-
-    //let usuario = await req.json()
-    let userFound = await User.findOne({email})
-
-    userFound.chat_history = []
-    const userUpdated = await User.findOneAndUpdate({ email }, userFound, {
-      new: true
-    });
-
-    console.log(userUpdated)
-
-    return NextResponse.json(userUpdated)
-
-  } catch (error: any) {
-
-    console.log(error.message)
-
-    return NextResponse.json(error.message, {
-      status: 400
-    })
-  }
-}

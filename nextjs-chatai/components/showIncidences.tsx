7bc4b5a16f37f5983cdb24a80e4558ee62881562
jorgerr9011 <jorge.rodriguez.rivera@udc.es>
2024-05-29 18:33:14 +0200

    Añadida funcionalidad para ordenar y filtrar por atributos de la tabla incidencias

--- nextjs-chatai/components/showIncidences.tsx
+++ nextjs-chatai/components/showIncidences.tsx
@@ -1,19 +1,24 @@
 'use client'
 
 import Loading from '@/components/loading'
-import Incidence from '@/models/Incidence'
-import Incidencia from '@/components/incidence'
+import { Incidencia } from '@/types/Incidence'
+import Incidence from '@/components/incidence'
 import React, { useEffect, useState } from 'react'
 import { useIncidences } from '../app/hooks/useIncidences'
 import { useRouter } from 'next/navigation'
 import { useSession } from 'next-auth/react'
 import { ObjectId } from 'mongoose'
 import { Usuario } from '@/types/User'
+import Select from 'react-select'
+import { getOrder } from '@/lib/order'
 
-export default function ShowIncidences({allIncidences}: {allIncidences: boolean}) {
+export default function ShowIncidences({ allIncidences }: { allIncidences: boolean }) {
 
     const router = useRouter()
-    const { isLoading, listIncidences } = useIncidences({allIncidences})
+    const { isLoading, listIncidences } = useIncidences({ allIncidences })
+    const [selectedStatus, setSelectedStatus] = useState<string | null>(null)
+    const [elementOrder, setElementOrder] = useState<string>("createdAt")
+    const [order, setOrder] = useState<string>("ascendente")
     const [currentPage, setCurrentPage] = useState(0)
     const [search, setSearch] = useState('')
     const [loading, setLoading] = useState(true)
@@ -36,7 +41,7 @@ export default function ShowIncidences({allIncidences}: {allIncidences: boolean}
         let user = usuario
         user.open_incidences_count = 0
         setUsuario(user)
-    } 
+    }
 
     const handleDelete = async () => {
         if (window.confirm("¿Estas seguro de querer borrar esta incidencia?")) {
@@ -72,12 +77,36 @@ export default function ShowIncidences({allIncidences}: {allIncidences: boolean}
             setCurrentPage(currentPage - 5)
     }
 
-    const filteredIncidences = (): typeof Incidence[] => {
-        if (search.length === 0) {
-            return listIncidences.slice(currentPage, currentPage + 5)
+    const filteredIncidences = (): Incidencia[] => {
+
+        let listInc: Incidencia[] = listIncidences
+        listInc = getOrder(listInc, order, elementOrder)
+
+        if (selectedStatus !== "null") {
+            listInc = listInc.filter((inci: Incidencia) => inci.status.includes(selectedStatus ? selectedStatus : ""))
         }
 
-        return listIncidences.filter((inci: typeof Incidence) => inci.name.includes(search))
+        if (search.length === 0) {
+            return listInc.slice(currentPage, currentPage + 5)
+        }
+
+        listInc = listInc.filter((inci) => inci.name.includes(search))
+        return listInc.slice(currentPage, currentPage + 5)
+    }
+
+    const handleStatusFilterChange = (selectedOption: { value: string } | any) => {
+        setCurrentPage(0)
+        setSelectedStatus(selectedOption.value)
+    }
+
+    const handleElementFilterChange = (selectedOption: { value: string } | any) => {
+        setCurrentPage(0)
+        setElementOrder(selectedOption.value)
+    }
+
+    const handleOrderFilter = (selectedOption: { value: string } | any) => {
+        setCurrentPage(0)
+        setOrder(selectedOption.value)
     }
 
     const onSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -103,16 +132,65 @@ export default function ShowIncidences({allIncidences}: {allIncidences: boolean}
 
     }, [status])
 
+    const statusOptions = [
+        { value: null, label: 'Cualquier incidencia' },
+        { value: 'OPEN', label: 'Abiertas' },
+        { value: 'CLOSED', label: 'Cerradas' },
+    ]
+
+    const orderOptions = [
+        { value: 'createdAt', label: 'Fecha creación' },
+        { value: 'name', label: 'Nombre incidencia' },
+        { value: 'status', label: 'Estado' },
+    ]
+
     return (
         <div className="grid grid-cols-1">
             {isLoading === false && loading === false ? (
                 <div className="container mx-auto">
-                    <input
-                        type="text"
-                        className="mb-5 form-control rounded-md text-black flex-grow w-5/12 border border-gray-400 focus:border-red-400"
-                        placeholder='Búsqueda de incidencia'
-                        value={search}
-                        onChange={onSearchChange} /><table className="min-w-full bg-white shadow-md rounded-lg overflow-hidden">
+                    <div className='flex flex-col gap-2 py-2'>
+                        <input
+                            type="text"
+                            className="pl-4 form-control rounded-md text-black flex-grow w-5/12 border border-gray-400 focus:border-red-400"
+                            placeholder='Búsqueda de incidencia'
+                            value={search}
+                            onChange={onSearchChange} />
+
+                        <div className='flex flex-row items-start'>
+
+                            <div className='flex flex-col w-3/12'>
+                                <label className='px-2'>Ordenación:</label>
+                                <select className='pl-4 py-2' id="ordenamiento" onChange={handleOrderFilter}>
+                                    <option value="ascendente">Ascendente</option>
+                                    <option value="descendente">Descendente</option>
+                                </select>
+                            </div>
+
+                            <div className='flex flex-col w-4/12 pl-4'>
+                                <label className='px-2'>Filtrar por:</label>
+                                <Select
+                                    id="orderFilter"
+                                    className="block text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+                                    options={orderOptions}
+                                    onChange={handleElementFilterChange}
+                                    defaultValue={orderOptions[0]}
+                                />
+                            </div>
+
+                            <div className='flex flex-col w-4/12 pl-4'>
+                                <label className='px-2'>Filtrar por estado:</label>
+                                <Select
+                                    id="statusFilter"
+                                    className="block text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+                                    options={statusOptions}
+                                    onChange={handleStatusFilterChange}
+                                    defaultValue={statusOptions[0]}
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+                    <table className="my-4 min-w-full bg-white shadow-md rounded-lg overflow-hidden">
                         <thead className="bg-gray-800 text-white">
                             <tr>
                                 <th className="py-2 px-4 text-left">Nombre</th>
@@ -124,8 +202,8 @@ export default function ShowIncidences({allIncidences}: {allIncidences: boolean}
 
                         {usuario != undefined ? (
                             <tbody>
-                                {filteredIncidences().map((item: any) => (
-                                    <Incidencia key={item._id} incidencia={item}/>
+                                {filteredIncidences().map((item: Incidencia) => (
+                                    <Incidence key={item._id} incidence={item} />
                                 ))}
                             </tbody>
                         ) : (

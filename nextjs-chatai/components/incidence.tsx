a607e9001147db1e085c498e1512e805ae6af0c5
jorgerr9011 <jorge.rodriguez.rivera@udc.es>
2024-05-30 17:40:59 +0200

    Cambio en ruta de funcionalidad modifyIncidence

--- nextjs-chatai/components/incidence.tsx
+++ nextjs-chatai/components/incidence.tsx
@@ -1,7 +1,18 @@
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 export default function Incidence({ incidence }: any) {
     
+    const router = useRouter()
+
+    const handleClick = () => {
+        
+        if (incidence.status === 'OPEN') {
+
+            router.push('/myIncidences/modifyIncidence/'+incidence._id)
+        }
+    }
+
     return (
         <tr>
             <td className="py-2 px-4">
@@ -12,7 +23,7 @@ export default function Incidence({ incidence }: any) {
             <td className="py-2 px-4">{incidence.description}</td>
             <td className="py-2 px-4">{incidence.status}</td>
             <td className="py-2 px-4">{new Date(incidence.createdAt).toLocaleString()}</td>
-            <td className="py-2 px-4"><Link href="/myIncidences/modifyIncidence">editar</Link></td>
+            <td className="py-2 px-4"><a href='' onClick={handleClick}>editar</a></td>
         </tr>
     );
 }

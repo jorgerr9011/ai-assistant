527291eedd1cee213a9713009b7e1d158bfa543c
jorgerr9011 <jorge.rodriguez.rivera@udc.es>
2024-12-28 13:15:01 +0100

    Creación de carpeta /icons en components para abstraelo del resto de componentes.

--- ai-assistant-frontend/components/dashboard.tsx
+++ ai-assistant-frontend/components/dashboard.tsx
@@ -8,6 +8,13 @@ import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
 import { Usuario } from "@/types/User";
 import ConfirmDialog from "@/components/confirm";
+import { DesplegableIcon } from "@/components/icons/DesplegableIcon";
+import { ChatsIcon } from "@/components/icons/chats/ChatsIcon";
+import { CreateChatIcon } from "./icons/chats/CreateChatIcon";
+import { PreviousPageIcon } from "./icons/chats/PreviousPageIcon";
+import { NextPageIcon } from "./icons/chats/NextPageIcon";
+import { EditChatIcon } from "./icons/chats/EditChatIcon";
+import { DeleteChatIcon } from "./icons/chats/DeleteChatIcon";
 
 export default function Dashboard() {
   const { data: session, status } = useSession({
@@ -278,20 +285,7 @@ export default function Dashboard() {
                 onClick={handleDesplegable}
                 className="rounded-full p-3 h-12 hover:bg-pink-400"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="size-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                  />
-                </svg>
+                <DesplegableIcon />
               </button>
             </div>
             <div
@@ -304,20 +298,7 @@ export default function Dashboard() {
                   className="text-black font-semibold p-3 hidden md:block rounded-lg hover:bg-pink-400"
                 >
                   <p className="flex justify-center">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      className="size-6 mr-2"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 0 1-.825-.242m9.345-8.334a2.126 2.126 0 0 0-.476-.095 48.64 48.64 0 0 0-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0 0 11.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155"
-                      />
-                    </svg>
+                    <CreateChatIcon />
                     {desplegable === "70%" ? <>Crear chat</> : <></>}
                   </p>
                 </button>
@@ -330,14 +311,7 @@ export default function Dashboard() {
                     className="text-black font-semibold p-3 md:block hidden rounded-lg hover:bg-pink-400"
                   >
                     <p className="flex justify-center">
-                      <svg
-                        className="mr-2 h-5 text-black"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z" />
-                      </svg>
+                      <ChatsIcon />
                       {desplegable === "70%" ? <>Mis chats</> : <></>}
                     </p>
                   </button>
@@ -365,39 +339,15 @@ export default function Dashboard() {
                               }
                               className="hidden md:block rounded-xl hover:bg-blue-500"
                             >
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth="1.5"
-                                stroke="currentColor"
-                                className="size-6"
-                              >
-                                <path
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                  d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                                />
-                              </svg>
+                              <DeleteChatIcon />
                             </Link>
-                            <button className="hidden md:block rounded-xl hover:bg-blue-500">
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                onClick={() =>
-                                  handleModalEdit(item._id as unknown as String)
-                                }
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth="1.5"
-                                stroke="currentColor"
-                                className="size-6"
-                              >
-                                <path
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                  d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
-                                />
-                              </svg>
+                            <button
+                              onClick={() =>
+                                handleModalEdit(item._id as unknown as String)
+                              }
+                              className="hidden md:block rounded-xl hover:bg-blue-500"
+                            >
+                              <EditChatIcon />
                             </button>
                           </li>
                         ))}
@@ -408,20 +358,7 @@ export default function Dashboard() {
                         onClick={previousPage}
                         className="button button-primary md:block hidden rounded-xl m-4 hover:bg-blue-500"
                       >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke-width="1.5"
-                          stroke="currentColor"
-                          className="size-6"
-                        >
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="m18.75 4.5-7.5 7.5 7.5 7.5m-6-15L5.25 12l7.5 7.5"
-                          />
-                        </svg>
+                        <PreviousPageIcon />
                       </button>
                       <label className="mt-4 md:block hidden">
                         {" "}
@@ -431,20 +368,7 @@ export default function Dashboard() {
                         onClick={nextPage}
                         className="button button-primary md:block hidden rounded-xl m-4 hover:bg-blue-500"
                       >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke-width="1.5"
-                          stroke="currentColor"
-                          className="size-6"
-                        >
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5"
-                          />
-                        </svg>
+                        <NextPageIcon />
                       </button>
                     </div>
                   </div>
